e1fa4fda866d2f34424a0aaa2cba47c55a5a9dc9
Luke Miloszewski <lukemiloszewski@gmail.com>
2021-11-14 09:06:40 +0200

    Update git commands

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,6 @@ import subprocess
 
 if __name__ == "__main__":
     if "{{ cookiecutter.init_git_repo }}" == "yes":
-        subprocess.call(['git', 'init', '--branch', 'main'])
-        subprocess.call(['git', 'add', '--all'])
-        subprocess.call(['git', 'commit', '--message', "🎉 Initial commit"])
+        subprocess.call(['git', 'init', '-b', 'main'])
+        subprocess.call(['git', 'add', '-A'])
+        subprocess.call(['git', 'commit', '-m', "🎉 Initial commit"])
